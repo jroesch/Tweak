9b282236190b872e1dcdaead799e25007fc6aa92
Jared Roesch <roeschinc@gmail.com>
2013-02-09 02:41:02 -0800

    added project config

--- project/GLLBuild.scala
+++ project/GLLBuild.scala
@@ -2,9 +2,21 @@ import sbt._
 import Keys._
 
 object GLLBuild extends Build {
-  lazy val tweak = Project("Tweak", file("."),
-    settings = Defaults.defaultSettings
-  ) dependsOn(gllCombinators)
 
-  lazy val gllCombinators = uri("https://github.com/jroesch/gll-combinators")
+  lazy val root = 
+    Project("Tweak", file("."),
+      settings = mainSettings
+    ) dependsOn(gllCombinators)
+    
+  lazy val gllCombinators = 
+    uri("https://github.com/jroesch/gll-combinators")
+    
+  lazy val mainSettings: Seq[Project.Setting[_]] = Defaults.defaultSettings ++ Seq(
+    name := "Tweak-Language",
+    version := "alpha-1.0",
+    scalaVersion := "2.10.0",
+    libraryDependencies ++= Seq (
+      "org.scala-lang" % "jline" % "2.10.0"
+    )
+  )
 }
