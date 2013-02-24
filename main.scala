77ba092696404ba5aa9fa1a22b7f3e540a0711b3
Jared Roesch <roeschinc@gmail.com>
2013-02-23 20:04:34 -0800

    updates repl to use jline
    Removed the old server approach, thanks to fixed line buffering inside of SBT.

--- main.scala
+++ main.scala
@@ -1,35 +1,20 @@
-//import unfiltered.request._
-//import unfiltered.response._
-import tweak.compiler.TParser
+import tweak.compiler.frontend.{ TParser => TweakParser }
 import scala.tools.jline.console.ConsoleReader
 import scala.io.Source._
-
 
 object Main {
   def main(args: Array[String]) {
     //unfiltered.jetty.Http.local(8080).filter(REPLServer).run()
-    val reader = new ConsoleReader();
-    reader.setPrompt("> ")
-    var line = reader.readLine()
-    while (line != null) {
-      println(TParser.parseAll(TParser.all, line))
-      line = reader.readLine()
+    if (args.length > 0) {
+      println(TweakParser.program(fromFile(args(0)).mkString))
+    } else {
+      val reader = new ConsoleReader();
+      reader.setPrompt("> ")
+      var line = reader.readLine()
+      while (line != null) {
+        println(TweakParser.dec(line).head)
+        line = reader.readLine()
+      }
     }
   } 
 }
-
-/* object REPLServer extends unfiltered.filter.Plan {
-  def intent = {
-    case req @ Path("/") => req match {
-      case POST(_) => {
-        val Params(params) = req
-        val result = repl(params("code").mkString)
-        Ok ~> ResponseString(result)
-      }
-    }
-  }
-  
-  def repl(s: String): String = {
-    TParser.interpret(s).toString
-  }
-} */
