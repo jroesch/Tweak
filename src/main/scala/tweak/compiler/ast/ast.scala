d91b0355a2d1fd1e7e0ef0d6e2bb69cd49b09a7a
Jared Roesch <roeschinc@gmail.com>
2013-02-23 23:39:19 -0800

    adds more nodes to the AST, renames Node => Term

--- src/main/scala/tweak/compiler/ast/ast.scala
+++ src/main/scala/tweak/compiler/ast/ast.scala
@@ -1,38 +1,67 @@
 package tweak.compiler
 
+import language.implicitConversions
+
 package object ast {
-  sealed trait Node
+  
+  trait PrettyPrintable {
+    def pprint: String = this.toString
+  }
+  
+  sealed trait Term extends PrettyPrintable
+  
+  /* Program */
+  case class Program(es: Seq[Binding]) extends Term
   
   /* Expressions */
-  sealed trait Exp extends Node
-  case class BinOp extends Exp
-  case class CaseExp(scrutinee: Any, binders: Any) extends Exp
+  sealed trait Exp extends Term
+  case class Op(op: Symbol) extends Exp
+  case class CaseExp(scrutinee: Any, matches: Any) extends Exp
   case class AssignExp(name: Any, value: Any) extends Exp
   
   /* Functions */
-  case class Function(params: Seq[Any], body: Seq[Exp]) extends Node
-  case class FunctionCall(callee: Function, args: Seq[Exp])
+  case class TFunction(ms: Seq[Match]) extends Exp
+  
+  case class Apply(a: Exp, b: Exp) extends Exp {
+    override def pprint = a match {
+      case Apply(Apply(op: Op, e1), e2) => 
+        e1.pprint + " " + op.pprint + " " + e2.pprint
+      case Apply(f, g) => f.pprint + " " + g.pprint
+    }
+    
+    // Shift reduce parser for operators?
+    /* case (level, Apply(Apply(op, e1) e2)) => self(level, e2)
+    case (level, e) => */
+    
+    def applyTo(e: Exp): Apply = Apply(this, e)
+  }
+  
+  
+  /* Match */
+  case class Match(pat: Pattern, exp: Exp) extends Term
+  case class MatchSeq(ps: Seq[Pattern]) extends Term
   
   /* Pattern */
-  sealed trait Pattern extends Node
+  sealed trait Pattern extends Term
   case class LiteralPattern(l: Literal) extends Pattern
-  case class PatternSeq(pat: Seq[Node]) extends Pattern
+  case class PatternSeq(pat: Seq[Term]) extends Pattern
   case class IdPattern(id: Id) extends Pattern
-  case object WildCard extends Pattern
+  case object WildCardPattern extends Pattern
+  case class ConsPattern(head: Pattern, tail: Pattern) extends Pattern
   
   /* Identifier */
-  case class Symbol(s: String) extends Node
-  case class Id(s: Symbol) extends Node
+  case class Symbol(s: String) extends Term
+  case class Id(s: Symbol) extends Exp
   
   /* Variable */
-  case class Binding(pat: Pattern, typ: Type = AnyType, exp: Exp)
+  case class Binding(pat: Pattern, typ: Type = AnyType, exp: Exp) extends Term
   
   /* Type */
-  sealed trait Type extends Node
+  sealed trait Type extends Term
   case object AnyType extends Type
   
   /* Literals */
-  sealed trait Literal extends Node
+  sealed trait Literal extends Exp
   sealed trait Number extends Literal
   case class IntL(i: Int) extends Number
   case class DoubleL(d: Double) extends Number
