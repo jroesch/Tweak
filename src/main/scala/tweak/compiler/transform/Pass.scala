d3e9bff8664192ec208edeb5a6c502b62286cd59
Jared Roesch <roeschinc@gmail.com>
2013-02-27 01:50:09 -0800

    adds stub for compiler Passes

---
+++ src/main/scala/tweak/compiler/transform/Pass.scala
@@ -0,0 +1,12 @@
+package tweak.compiler.transform
+
+/* Interface for a Compiler Pass */
+trait Pass {
+  
+}
+
+/* Ideas is
+  Phase Type: Common, Pre-Scala Phase, Pre-JS Phase, Rewrite, Optimization 
+ */
+ 
+ 
