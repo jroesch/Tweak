fa525dc15ad1265dfa7d079a105344a5eee5bb6b
Jared Roesch <roeschinc@gmail.com>
2013-02-27 01:14:33 -0800

    removes old ast boilerplate

--- src/main/scala/tweak/compiler/ast/ArgListNode.scala
+++
@@ -1,3 +0,0 @@
-/* package tweak.compiler.ast 
-
-class ArgListNode[T](s: Seq[T]) extends ListNode[T](s) */
--- src/main/scala/tweak/compiler/ast/BaseNode.scala
+++
@@ -1,6 +0,0 @@
-package tweak.compiler.ast
-import tweak.internal.types._
-
-trait BaseNode {
-  //def eval[T]: T 
-}
