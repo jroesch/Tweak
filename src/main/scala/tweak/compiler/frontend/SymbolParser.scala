68d680ad99c8d9fededd5c422b3c0eba953e13e3
Jared Roesch <roeschinc@gmail.com>
2013-03-13 20:45:55 -0700

    fixes a all compilation issues with new SymbolParser

--- src/main/scala/tweak/compiler/frontend/SymbolParser.scala
+++ src/main/scala/tweak/compiler/frontend/SymbolParser.scala
@@ -4,16 +4,24 @@ import language.postfixOps
 import com.codecommit.gll._
 import tweak.compiler.ast._
 
+trait SymbolParser extends TweakParser {
+  val symbol: GLLParser[Symbol] 
+  val id: GLLParser[Id]
+  val op: GLLParser[Op]
+}
 
-/* find better naming */
-trait Name extends TweakParser {
-  lazy val name: GLLParser[Var] = (
+trait DefaultSymbolParser extends SymbolParser {
+  lazy val symbol = (
       id
     | op
   )
-  
+
+  lazy val id = identifier ^^ { i => Id(i) }
+
+  lazy val op = operator ^^ { o => Op(o) }
+
   val operator: GLLParser[String] = """[=\+\*\-\?\\\$<>:]+"""r
   
   val identifier: GLLParser[String] = """[a-zA-Z]([a-zA-Z0-9]|_[a-zA-Z0-9])*"""r
-
 }
+  
