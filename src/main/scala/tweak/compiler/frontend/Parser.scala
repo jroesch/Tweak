01f4fb9849f808c01ecb7580addfc21aa628937a
Jared Roesch <roeschinc@gmail.com>
2013-03-14 20:59:25 -0700

    testing op parsing in the Parser

--- src/main/scala/tweak/compiler/frontend/Parser.scala
+++ src/main/scala/tweak/compiler/frontend/Parser.scala
@@ -3,18 +3,20 @@ package tweak.compiler.frontend
 import com.codecommit.gll._
 import tweak.compiler.ast._
 import language.postfixOps
+import scala.language.implicitConversions
 
 object Parser extends DefaultExpParser
     with DefaultFunctionParser
     with DefaultMatchParser
     with DefaultSymbolParser
-    with DefaultLiteralParser {
+    with DefaultLiteralParser 
+    with OpParser {
   /* implicit class UnwrapResult[A <: Term](val r: Result[A]) extends AnyVal {
     def result: Term = r match {
       case Success
     }
   } */
-
+  
   override val whitespace = """(\s|\(\*([^*]|\*[^)])*\*\))+"""r
   
   lazy val interpreter = exp | dec
@@ -22,7 +24,13 @@ object Parser extends DefaultExpParser
   lazy val program: GLLParser[Program] = (dec*) ^^ { ds => Program(ds) }
   
   lazy val decs = dec*
-  
+ 
+  lazy val infix: GLLParser[_] = (
+      "infix" ~ symbol ~ int ^^ { (_, sym, i) => precTable(sym.name) = (i.toInt, N) }
+    | "infixl" ~ symbol ~ int ^^ { (_, sym, i) => precTable(sym.name) = (i.toInt, L) }
+    | "infixr" ~ symbol ~ int ^^ { (_, sym, i) => precTable(sym.name) = (i.toInt, R) }
+  )
+
   lazy val dec: GLLParser[Binding] = (
       "val" ~ pattern ~ "=" ~ exp ^^ { (_, p, _, e) => Binding(pat = p, exp = e) }
     | "val" ~ id ~ "=" ~ function ^^ { 
